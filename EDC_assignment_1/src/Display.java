3af9d234e7fbc307ac70e7f29faff954a0784c53
happybuddha7 <s.p.hennessy7@gmail.com>
2017-08-08 14:07:30 +0930

    Add files via upload

--- EDC_assignment_1/src/Display.java
+++ EDC_assignment_1/src/Display.java
@@ -1,56 +1,92 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Display extends JFrame implements Gui 
 {
+	//controller
+	private Controller controller;
+	//other view variables
 	private JTextField text_string;
 	private JButton insert_button;
 	private JButton go_button;
-	
+	private JTextField time_field;
+
+	//constructor
 	public Display(){
-	super("Title bar");
-
-	}
-	
-	  //Connect gui to controller
-    //(This method will be called before ANY other methods)
-    public void connect(Controller controller){
-
-
-	}
-
-    //Initialise the gui
-    public void init(){
+		super("Title bar");
+		//Layout
 		setLayout(new FlowLayout());
-		//string
+		//String
 		text_string = new JTextField("Waiting...");
+		time_field = new JTextField("0.0"+" "+"ms");
+		//Buttons
+		insert_button	= new JButton("INSERT COIN");
+		go_button = new JButton("GO!/STOP!");
+		//add ui elements
 		add(text_string);
-		//buttons
-		insert_button	= new JButton("INSERT");
-		go_button = new JButton("GO!");
 		add(insert_button);
 		add(go_button);
+		add (time_field);
+
+		//Event Listeners
+		//insert_button.addActionListener(connect(Controller controller));
+		//go_button.addActionListener(onClick);
 
 
-		insert_button.addActionListener(onClick);
-		go_button.addActionListener(onClick);
+
 		//display properties
 		setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500,500);
+	}
 
+	//Connect gui to controller
+    //(This method will be called before ANY other methods)
+    public void connect(Controller controller){
+		this.controller = controller;
+
+		//action listeners
+		insert_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controller.coinInserted();
+			}
+		});
+
+		go_button.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			controller.goStopPressed();
+		}
+		});
+	}
+
+    //Initialise the gui
+    public void init(){
+		text_string.setText("Insert coin");
+		insert_button.setText("INSERT COIN");
+		go_button.setText("GO/STOP!");
+		time_field.setText("0.0"+" "+"ms");
 		//event handler
-		eventHandler onClick = new eventHandler();
-
 	}
 
     //Change the displayed text
     public void setDisplay(String s){
-	text_string = new JTextField("Not Waiting");
+    	if (s=="Press GO!") {
+			text_string.setText(s);
+		}
+		else if (s=="Wait...") {
+			text_string.setText(s);
+		}
+		else
+		{
+			time_field.setText(s);
+		}
 	}
 
-	private class eventHandler implements ActionListener
+
 
 
 }
--- EDC_assignment_1/src/SimpleReactionController.java
+++ EDC_assignment_1/src/SimpleReactionController.java
@@ -1,29 +1,112 @@
 
 public class SimpleReactionController implements Controller
-{
+{   //MVC members
+    private ReactionMachine model;
+    private Gui view;
+    private Random random;
+    //other members
+    private int credits;
+    private int delay=0;
+    private double delay_timer=0;
+    private double time=0;
+    private int score_display_timer=0;
+    private String string_time;
+    private boolean timer_on=false;
+    private boolean game_in_progress = false;
+
+
     //Connect controller to gui
     //(This method will be called before ANY other methods)
 	
 	public SimpleReactionController(){
-	
+
 	}
 	
     public void connect(Gui gui, Random rng){
+        this.random = rng;
+	    this.view = gui;
 	}
-
-    //Called to initialise the controller
+	//Called to initialise the controller
     public void init(){
-	}
+        delay=0;
+        delay_timer=0 ;
+        time=0;
+        score_display_timer=0;
+        timer_on=false;
+        game_in_progress = false;
 
+    }
     //Called whenever a coin is inserted into the machine
     public void coinInserted(){
+        credits = credits +1;
+        view.setDisplay("Press GO!");
 	}
 
     //Called whenever the go/stop button is pressed
     public void goStopPressed(){
+        //if credits available:
+        if (credits>0){
+            //if timer has not started, start timer
+            if (timer_on==false) {
+                view.setDisplay("Wait...");
+                delay=random.getRandom(1000,2500);
+                delay_timer=0;
+                time=0;
+                timer_on=true;
+                game_in_progress=true;
+
+            }
+            //if timer is running, stop timer, remove a credit
+            //keep time on display, reset time
+            else if (timer_on==true){
+                //if button is pressed before timer starts
+                if(delay_timer<delay){
+                    credits=credits-1;
+                    view.init();
+                    this.init();
+                }//did not cheat: stop timer, display final time reset after 3 seconds
+                else {
+                    timer_on = false;
+                    credits = credits - 1;
+                    view.setDisplay(string_time);
+                }
+            }
+        }
 	}
 
     //Called to deliver a TICK to the controller
     public void tick(){
+
+        if (timer_on==true) {
+           //if timer hits 2 seconds without response
+            if(time>=2){
+                timer_on = false;
+                credits = credits-1;
+                score_display_timer=score_display_timer+10;
+                view.setDisplay("2");
+                if (score_display_timer>=3000) {
+                    this.init();
+                    view.init();
+                }
+            }
+            //when the delay_timer has reached the random delay period
+            //start reaction_timer
+            delay_timer=delay_timer+10;
+            if(delay_timer>delay) {
+                time = time + 0.01;
+                string_time = String.format("%.1f", time);
+                //display the ticks
+                view.setDisplay(string_time);
+            }
+        }
+        else if (timer_on==false&& game_in_progress)//while the timer is off
+        {
+            score_display_timer=score_display_timer+10;
+            //view.setDisplay(string_time);
+            if (score_display_timer>=3000) {
+                this.init();
+                view.init();
+            }
+        }
 	}
 }
